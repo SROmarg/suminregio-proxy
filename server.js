6e808b6139b1352851b3d3a97683a5613e883371
SROmarg <omargonzalez@suminregio.com>
2026-02-24 13:42:19 -0600

    server.js

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ const http = require('http');
 const https = require('https');
 
 const server = http.createServer((req, res) => {
-  // CORS headers en todas las respuestas
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-api-key');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-api-key, anthropic-version');
 
   if (req.method === 'OPTIONS') {
     res.writeHead(200);
